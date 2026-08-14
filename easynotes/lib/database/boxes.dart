bdd26b59ebffcc8b20bb70f99e36200f18b4b84d
xpesta-studio <xxpesta@gmail.com>
2026-08-14 07:32:21 +0600

    Delete easynotes directory

--- easynotes/lib/database/boxes.dart
+++
@@ -1,5 +0,0 @@
-class HiveBoxes {
-  static const String notesBox = 'easynotes_notes_box';
-  static const String settingsBox = 'easynotes_settings_box';
-  static const String premiumBox = 'easynotes_premium_box';
-}
